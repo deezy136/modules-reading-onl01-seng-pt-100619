819ef0d51c682a7df5762ab5bbc11c05e248019c
Clyde Griffin <deezy13@gmail.com>
2019-11-30 03:20:13 +0000

    Automatically backed up by Learn

--- lib/entry_point.rb
+++ lib/entry_point.rb
@@ -5,16 +5,16 @@ require_relative './fancy_dance.rb'
 
 p angelina = Dancer.new
 p angelina.twirl
-// returns "I'm twirling!"
+#// returns "I'm twirling!"
 
 p angelina.jump
-// returns "Look how high I'm jumping!"
+#// returns "Look how high I'm jumping!"
  
 p buster = Kid.new
 
 p buster.jump
-// returns "Look how high I'm jumping!"
+#// returns "Look how high I'm jumping!"
 
 p buster.take_a_bow
-// returns "Thank you, thank you. It was a pleasure to dance for you all."
+#// returns "Thank you, thank you. It was a pleasure to dance for you all."
  
